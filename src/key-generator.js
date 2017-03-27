728ea019011d3f6d3ae71d29df611b287e68ac6d
Alexandr Murashkin <alexandr.murashkin@D2L.com>
2017-03-27 19:29:17 -0400

    Minor edits

--- src/key-generator.js
+++ src/key-generator.js
@@ -1,12 +1,13 @@
 'use strict';
 
-const	CoreKeyGenerator = require('./core-key-generator');
+const CoreKeyGenerator = require('./core-key-generator');
 
 function KeyGenerator(opts) {
 	if (typeof opts !== 'object') {
 		throw new TypeError(`"opts" should be an Object. Got "${typeof opts}".`);
 	}
 	this._coreKeyGenerator = new CoreKeyGenerator(opts);
+	this._keyGenerationTask = null;
 	this._currentPrivateKey = null;
 	this._generateNewKeys = this._generateNewKeys.bind(this);
 	this._generateNewKeys();
