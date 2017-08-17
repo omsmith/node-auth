52aaf522dd2852e7c28c2f1c97e9d762cd4abde5
Owen Smith <owen@omsmith.ca>
2017-08-17 15:35:15 -0400

    bin: fix test-all script for multiple packages

--- bin/test-all.sh
+++ bin/test-all.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
 DIRNAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-NODE_BIN=$(readlink -f "${DIRNAME}/../node_modules/.bin")
+ROOT=$(readlink -f "${DIRNAME}/..")
+NODE_BIN=$(readlink -f "${ROOT}/node_modules/.bin")
 
 run_tests () {
+	local root="${1}"; shift
 	local package="${1}"; shift
-
-	package=$(readlink -f "${package}")
 
 	cd "${package}"
 	npm test
+	cd "${root}"
 }
 
-for package in packages/node_modules/*/; do [ -d "${package}" ] && run_tests "${package}"; done
+for package in packages/node_modules/*/; do
+	package=$(readlink -f "${package}")
+	if [ ! -d "${package}" ]; then
+		continue
+	fi
+
+	run_tests "${ROOT}" "${package}"
+done
