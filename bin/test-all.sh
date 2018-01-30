fc30fa4ac070ca0cf8dc54f2ffe06267f55bc34f
Owen Smith <owen@omsmith.ca>
2018-01-30 17:06:20 -0500

    test: collect lcov info from nyc into common directory

--- bin/test-all.sh
+++ bin/test-all.sh
@@ -4,7 +4,18 @@ set -eu
 
 source "$(dirname ${BASH_SOURCE[0]})/helpers.sh"
 
+TOP_DIR=$(pwd)
+
 for package in $(get_packages); do
 	cd "${package}"
 	npm test
+
+	cd "${TOP_DIR}"
+	if [ -d "${package}/.nyc_output" ]; then
+		./node_modules/.bin/nyc \
+			report \
+			--temp-directory "${package}/.nyc_output" \
+			--reporter lcov \
+			--report-dir "coverage/$(basename "${package}")"
+	fi
 done
