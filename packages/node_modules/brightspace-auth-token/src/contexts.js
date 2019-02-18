c79f3463729018d907735b110591a963f4003fcd
Owen Smith <owen@omsmith.ca>
2019-02-17 21:24:12 -0500

    assertions, token: use strings to refer to context types
    assertions: improve context error message
    
    Closes: https://github.com/Brightspace/node-auth/issues/41
    PR-URL: https://github.com/Brightspace/node-auth/pull/107

--- packages/node_modules/brightspace-auth-token/src/contexts.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = Object.freeze({
-	Global: Symbol('BrightspaceAuthToken-Context-Global'),
-	Tenant: Symbol('BrightspaceAuthToken-Context-Tenant'),
-	User: Symbol('BrightspaceAuthToken-Context-User')
-});
